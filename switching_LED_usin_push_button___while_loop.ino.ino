b5da8c8d0af7e634e71898d1550503e817e9b6bf
Mostafa-Abdelnasser <90975315+Mostafa-Abdelnasser@users.noreply.github.com>
2026-02-07 15:41:59 +0200

    Add files via upload

---
+++ switching_LED_usin_push_button___while_loop.ino.ino
@@ -0,0 +1,23 @@
+
+int buttonPin = 2;      // the number of the pushbutton pin is 2
+ int ledPin =  13;      // the number of the LED pin is 13
+
+// variables will change:
+int buttonState = 0;   // variable for reading the pushbutton status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+void loop() {
+  // read the state of the pushbutton value:
+        digitalWrite(ledPin, LOW);
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  while (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);     // turn LED on
+    break;
+  }
+  delay(100);
+}
